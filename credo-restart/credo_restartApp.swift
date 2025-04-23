d8cd6c14c2525d17417ce0c97037bdcb887d69a4
Alan Milke <pojo@pojoclan.com>
2025-04-23 14:07:18 -0600

    Initial Commit

---
+++ credo-restart/credo_restartApp.swift
@@ -0,0 +1,17 @@
+//
+//  credo_restartApp.swift
+//  credo-restart
+//
+//  Created by Alan Milke on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct credo_restartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
